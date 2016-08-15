9bfb460a05d1d65d4812a6445c6c331bb1fe99cb
ferbarragan <ferbarragan.developer@gmail.com>
2016-08-15 18:39:30 -0500

    - Configuración inicial. - Importación de librería SQLite. - Creación de una clase para el manejo de la base de datos.

---
+++ ControlDeGastos/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  ControlDeGastos
+//
+//  Created by Christian Barragan on 15/08/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ ControlDeGastos/DBManager.m
@@ -0,0 +1,13 @@
+//
+//  DBManager.m
+//  ControlDeGastos
+//
+//  Created by Christian Barragan on 15/08/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+@end
