5fb3533fe313f08bcd5dfba1ad987f45067cffc0
ferbarragan <ferbarragan.developer@gmail.com>
2016-08-15 18:31:53 -0500

    Initial Commit

---
+++ ControlDeGastos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlDeGastos
+//
+//  Created by Christian Barragan on 15/08/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
