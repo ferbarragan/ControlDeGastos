337f45dfe5a27a5fc5c8f2ca2291e4842b1c9bc2
ferbarragan <ferbarragan.developer@gmail.com>
2016-08-15 20:58:06 -0500

    Métodos para el control de la base de datos implementados.

--- ControlDeGastos/DBManager.h
+++ ControlDeGastos/DBManager.h
@@ -10,4 +10,14 @@
 
 @interface DBManager : NSObject
 
+-(instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+@property (nonatomic) int affectedRows;
+@property (nonatomic) long long lastInsertedRowID;
+
+-(NSArray *)loadDataFromDB:(NSString *)query;
+-(void)executeQuery:(NSString *)query;
+
+
 @end
